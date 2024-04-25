2b2134becddccab3dc2598c206ccc95450366b16
tbinh <96913224+DACMI@users.noreply.github.com>
2024-04-25 20:44:40 +0700

    Add files via upload

---
+++ song.js
@@ -0,0 +1,69 @@
+let songs = [
+    {
+        name: '11:11' , 
+        path: 'music/y2mate.com - 1111 11 giờ 11 phút  MiiNa x RIN9 x DREAMeR  MV Lyrics_320kbps.mp3' , 
+        artist: 'MiiNa x RIN9 x DREAMeR' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Anh sao va bau troi' , 
+        path: 'music/y2mate.com - Ánh Sao Và Bầu Trời  TRI x Cá  Official Audio_320kbps.mp3' , 
+        artist: 'TRI x Cá' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Dau ai dam hua' , 
+        path: 'music/y2mate.com - ĐÂU AI DÁM HỨA  CZEE Official Lyric MV  OST Thỏ Bảy Màu Và Người Yêu Mới Của Chị Xô_320kbps.mp3' , 
+        artist: 'CZEE' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Cham day noi dau' , 
+        path: 'music/y2mate.com - ERIK  CHẠM ĐÁY NỖI ĐAU MV ft MRSIRO_320kbps.mp3' , 
+        artist: 'Erik' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Giu lay lam gi' , 
+        path: 'music/y2mate.com - giu lay lam gi_320kbps.mp3' , 
+        artist: 'GreyD' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Ngay em dep nhat' , 
+        path: 'music/y2mate.com - Ngày Em Đẹp Nhất_320kbps.mp3' , 
+        artist: 'Tama' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Khi em lon' , 
+        path: 'music/y2mate.com - Orange x Hoàng Dũng  Khi Em Lớn Official MV_320kbps.mp3' , 
+        artist: 'Orange x Hoàng Dũng' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Bong hoa dep nhat' , 
+        path: 'music/y2mate.com - QUÂN AP  BÔNG HOA ĐẸP NHẤT  OFFICIAL MUSIC VIDEO_320kbps.mp3' , 
+        artist: 'QUÂN AP' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Tinh co yeu em' , 
+        path: 'music/y2mate.com - TÌNH CỜ YÊU EM  Kuun Đức Nam ft Linh Thộn  Official Music Video  KEY ENTERTAINMENT_320kbps.mp3' , 
+        artist: 'Kuun Đức Nam ft Linh Thộn' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Tinh yeu cham tre' , 
+        path: 'music/y2mate.com - tinh yeu cham tre_320kbps.mp3' , 
+        artist: 'GreyD' , 
+        image : 'images/profile.jpg',
+    },
+    {
+        name: 'Album 3107' , 
+        path: 'y2mate.com - Wn  3107 full album ft  267 Nâu Dươngg _320kbps' , 
+        artist: 'Wn x 3107 ft  267 Nâu , Dươngg , Erik ' , 
+        image : 'images/profile.jpg',   
+    }
+]
+;
